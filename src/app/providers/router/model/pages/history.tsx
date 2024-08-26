033afbbe41978f739bec768a33bfadbd0807ad72
Vitaly Koshelkov <mail@koshelkov.ru>
2024-08-26 15:28:45 +0300

    delete HistoryPageLayoutProps

--- src/app/providers/router/model/pages/history.tsx
+++
@@ -1,8 +0,0 @@
-import { FooterTabs } from '@/entities/app-footer';
-import { ScreenLayoutProps } from '../types';
-
-export const HistoryPageLayoutProps: ScreenLayoutProps = {
-  Page: () => <></>,
-  headerTitle: 'My Cards',
-  tab: FooterTabs.HISTORY,
-};
--- src/app/providers/router/model/pages/index.ts
+++ src/app/providers/router/model/pages/index.ts
@@ -1,4 +1,3 @@
 export { MainPageLayoutProps } from './main';
 export { ErrorPageLayoutProps } from './error-page';
-export { HistoryPageLayoutProps } from './history';
 export { MainMenuPageLayoutProps } from './main-menu';
