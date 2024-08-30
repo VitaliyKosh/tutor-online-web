844f87e72f4ab2edc44c12de2f4d66f52038601b
Vitaly Koshelkov <mail@koshelkov.ru>
2024-08-31 02:29:04 +0300

    google experiment

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -22,40 +22,28 @@ self.addEventListener('push', function (e) {
 
 // Click and open notification
 self.addEventListener('notificationclick', function(event) {
-    event.notification.close();
+    // event.notification.close();
 
-    if (event.action === 'tests') clients.openWindow("/app/tests");
-    else if (event.action === 'home') clients.openWindow("/app/");
-    else if (event.action === 'google') clients.openWindow("https://google.com");
-    else if (event.action === 'advanced') {
-        event.waitUntil(
-            clients
-              .matchAll({
-                type: "window",
-              })
-              .then((clientList) => {
-                for (const client of clientList) {
-                  if (client.url === "/app" && "focus" in client) return client.focus();
+    // if (event.action === 'tests') clients.openWindow("/app/tests");
+    // else if (event.action === 'home') clients.openWindow("/app/");
+    // else clients.openWindow(event.action);
+
+    let url = 'https://google.com';
+    event.notification.close(); // Android needs explicit close.
+    event.waitUntil(
+        clients.matchAll({type: 'window'}).then( windowClients => {
+            // Check if there is already a window/tab open with the target URL
+            for (var i = 0; i < windowClients.length; i++) {
+                var client = windowClients[i];
+                // If so, just focus it.
+                if (client.url === url && 'focus' in client) {
+                    return client.focus();
                 }
-                if (clients.openWindow) return clients.openWindow("/app");
-              }),
-          );
-    }
-    else if (event.action === 'advanced2') {
-        event.waitUntil(
-            clients
-              .matchAll({
-                type: "window",
-              })
-              .then((clientList) => {
-                for (const client of clientList) {
-                  if (client.url === "/app/tests" && "focus" in client) return client.focus();
-                }
-                if (clients.openWindow) return clients.openWindow("/app/tests");
-              }),
-          );
-    }
-    else clients.openWindow(event.action);
-
-
+            }
+            // If not, then open the target URL in a new window/tab.
+            if (clients.openWindow) {
+                return clients.openWindow(url);
+            }
+        })
+    );
 }, false);
