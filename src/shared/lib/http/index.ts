3654218b06ae14235496a6a2ffd3bfecbc343a74
Vitaly Koshelkov <mail@koshelkov.ru>
2024-08-31 00:50:19 +0300

    edit flow

--- src/shared/lib/http/index.ts
+++ src/shared/lib/http/index.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosHeaders, AxiosResponse, InternalAxiosRequestConfig } from 'axios';
 import { LocalStorageItems } from '@/shared/types/local-storage-items';
 
-// export const API_URL = 'https://math.koshelkov.ru/api';
-export const API_URL = 'http://localhost:5010/api';
+export const API_URL = 'https://math.koshelkov.ru/api';
+// export const API_URL = 'http://localhost:5010/api';
 
 const $api = axios.create({
     withCredentials: true,
